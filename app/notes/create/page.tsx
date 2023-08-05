721f399caa673431a12ad5c7bb8531ca58f0641e
AmEhlee <ElieKabengele.Work@gmail.com>
2023-08-04 18:54:03 -0600

    Basic routing and styling

---
+++ app/notes/create/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <section>
+      <h2>Create Note Page</h2>
+    </section>
+  );
+};
+
+export default page;
---
+++ app/notes/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <section>
+      <h2>Note Page Base View</h2>
+    </section>
+  );
+};
+
+export default page;
